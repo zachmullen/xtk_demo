fe9fcd608522e479223ce067e194122512dfd181
Zach Mullen <zach.mullen@kitware.com>
2015-07-21 14:35:44 -0400

    Support for 2d volume

--- web_client/js/xtk_demo_XtkView.js
+++ web_client/js/xtk_demo_XtkView.js
@@ -7,19 +7,7 @@ girder.views.xtk_demo_XtkView = girder.View.extend({
 
     render: function () {
         this.$el.html(girder.templates.xtk_demo_viewer());
-        this.renderer = new X.renderer3D();
-        //this.renderer = new X.renderer2D();
-        //this.renderer.orientation = 'x';
-        this.renderer.container = this.$('.g-render-target')[0];
-        this.renderer.init();
-
-        var obj = this._instantiateObject();
-
-        if (obj) {
-            this.renderer.add(obj);
-            this.renderer.camera.position = [0, 400, 0];
-            this.renderer.render();
-        }
+        this._initXtkRenderer();
     },
 
     destroy: function () {
@@ -31,91 +19,148 @@ girder.views.xtk_demo_XtkView = girder.View.extend({
         girder.View.prototype.destroy.call(this);
     },
 
-    _instantiateObject: function () {
+    _initXtkRenderer: function () {
         var info = this.item.get('meta').XTK;
         var type = (info.type || 'mesh').toLowerCase();
+        var obj;
+
+        this._initGui();
 
         if (type === 'mesh') {
-            var mesh = new X.mesh();
-            var file = this.files.models[0];
-            var customProps = {
-                color: this._colorRangeUp(mesh.color)
-            };
-            mesh.file = file.downloadUrl() + '/' + file.name();
-            mesh.opacity = Number(info.opacity) || 1.0;
-
-            this._initGui();
-            var meshGui = this.gui.addFolder('Mesh display');
-            meshGui.add(mesh, 'opacity', 0, 1).name('Opacity');
-
-            meshGui.addColor(customProps, 'color').name('Color').onChange(_.bind(function (val) {
-                mesh.color = this._colorRangeDown(val);
-            }, this));
-            meshGui.open();
-            this.$('.hue-field').width(10); //hack to fix dat.gui colors in chrome
-
-            return mesh;
+            this.renderer = new X.renderer3D();
+            obj = this._doMesh(info);
+            window.obj = obj;
         } else if (type === 'volume') {
-            var volume = new X.volume();
-            volume.file = _.map(this.files.models, function (file) {
-                return file.downloadUrl() + '/' + file.name();
-            });
-
-            this.renderer.onShowtime = _.bind(function () {
-                volume.minColor = info.minColor || [0.0, 0.0, 0.0];
-                volume.maxColor = info.maxColor || [1.0, 1.0, 1.0];
-
-                if (_.has(info, 'opacity')) {
-                    volume.opacity = info.opacity;
-                }
-                if (_.has(info, 'window')) {
-                    volume.windowLower = info.window[0];
-                    volume.windowHigh = info.window[1];
-                }
-
-                volume.lowerThreshold = info.lowerThreshold || 0;
-
-                if (info.volumeRender) {
-                    volume.volumeRendering = true;
-                }
-
-                this._initGui();
-                var volumeGui = this.gui.addFolder('Volume display');
-                var customProps = {
-                    minColor: this._colorRangeUp(volume.minColor),
-                    maxColor: this._colorRangeUp(volume.maxColor)
-                };
-
-                volumeGui.add(volume, 'volumeRendering').name('Volume rendering');
-                volumeGui.add(volume, 'opacity', 0, 1).name('Opacity');
-                volumeGui.add(volume, 'lowerThreshold', volume.min, volume.max).name('Threshold min');
-                volumeGui.add(volume, 'upperThreshold', volume.min, volume.max).name('Threshold max');
-                volumeGui.add(volume, 'windowLow', volume.min, volume.max).name('Window min');
-                volumeGui.add(volume, 'windowHigh', volume.min, volume.max).name('Window max');
-                volumeGui.add(volume, 'indexX', 0, volume.range[0] - 1).name('X slice');
-                volumeGui.add(volume, 'indexY', 0, volume.range[1] - 1).name('Y slice');
-                volumeGui.add(volume, 'indexZ', 0, volume.range[2] - 1).name('Z slice');
-
-                // Dat.gui does not have built-in support for [0, 1] color ranges
-                volumeGui.addColor(customProps, 'minColor').name('Color min').onChange(_.bind(function (val) {
-                    volume.minColor = this._colorRangeDown(val);
-                }, this));
-                volumeGui.addColor(customProps, 'maxColor').name('Color max').onChange(_.bind(function (val) {
-                    volume.maxColor = this._colorRangeDown(val);
-                }, this));
-
-                volumeGui.open();
-                this.$('.hue-field').width(10); //hack to fix dat.gui colors in chrome
-
-            }, this);
-            return volume;
+            this.renderer = new X.renderer3D();
+            obj = this._doVolume(info);
+        } else if (type === 'volume2d') {
+            this.renderer = new X.renderer2D();
+            this.renderer.orientation = 'AXIAL';
+            obj = this._doVolume2d(info);
         } else {
             girder.events.trigger('g:alert', {
                 text: 'Unsupported XTK type: ' + type,
                 type: 'danger',
                 icon: 'cancel'
             });
+            return;
         }
+
+        this.renderer.container = this.$('.g-render-target')[0];
+        this.renderer.init();
+        this.renderer.add(obj);
+        // TODO compute from scene bounding box once it is available in the public API
+        this.renderer.camera.position = [0, 400, 0];
+        this.renderer.render();
+    },
+
+    _doMesh: function (info) {
+        var mesh = new X.mesh();
+        var file = this.files.models[0];
+        var customProps = {
+            color: this._colorRangeUp(mesh.color)
+        };
+        mesh.file = file.downloadUrl() + '/' + file.name();
+        mesh.opacity = Number(info.opacity) || 1.0;
+
+        var meshGui = this.gui.addFolder('Mesh display');
+        meshGui.add(mesh, 'opacity', 0, 1).name('Opacity');
+
+        meshGui.addColor(customProps, 'color').name('Color').onChange(_.bind(function (val) {
+            mesh.color = this._colorRangeDown(val);
+        }, this));
+        meshGui.open();
+        this.$('.hue-field').width(10); //hack to fix dat.gui colors in chrome
+
+        return mesh;
+    },
+
+    _doVolume: function (info) {
+        var volume = new X.volume();
+        volume.file = _.map(this.files.models, function (file) {
+            return file.downloadUrl() + '/' + file.name();
+        });
+
+        this.renderer.onShowtime = _.bind(function () {
+            volume.minColor = info.minColor || [0.0, 0.0, 0.0];
+            volume.maxColor = info.maxColor || [1.0, 1.0, 1.0];
+
+            if (_.has(info, 'opacity')) {
+                volume.opacity = info.opacity;
+            }
+            if (_.has(info, 'window')) {
+                volume.windowLower = info.window[0];
+                volume.windowHigh = info.window[1];
+            }
+
+            volume.lowerThreshold = info.lowerThreshold || 0;
+
+            if (info.volumeRender) {
+                volume.volumeRendering = true;
+            }
+
+            var volumeGui = this.gui.addFolder('Volume display');
+            var customProps = {
+                minColor: this._colorRangeUp(volume.minColor),
+                maxColor: this._colorRangeUp(volume.maxColor)
+            };
+
+            volumeGui.add(volume, 'volumeRendering').name('Volume rendering');
+            volumeGui.add(volume, 'opacity', 0, 1).name('Opacity');
+            volumeGui.add(volume, 'lowerThreshold', volume.min, volume.max).name('Threshold min');
+            volumeGui.add(volume, 'upperThreshold', volume.min, volume.max).name('Threshold max');
+            volumeGui.add(volume, 'windowLow', volume.min, volume.max).name('Window min');
+            volumeGui.add(volume, 'windowHigh', volume.min, volume.max).name('Window max');
+            volumeGui.add(volume, 'indexX', 0, volume.range[0] - 1).name('X slice');
+            volumeGui.add(volume, 'indexY', 0, volume.range[1] - 1).name('Y slice');
+            volumeGui.add(volume, 'indexZ', 0, volume.range[2] - 1).name('Z slice');
+
+            // Dat.gui does not have built-in support for [0, 1] color ranges
+            volumeGui.addColor(customProps, 'minColor').name('Color min').onChange(_.bind(function (val) {
+                volume.minColor = this._colorRangeDown(val);
+            }, this));
+            volumeGui.addColor(customProps, 'maxColor').name('Color max').onChange(_.bind(function (val) {
+                volume.maxColor = this._colorRangeDown(val);
+            }, this));
+
+            volumeGui.open();
+            this.$('.hue-field').width(10); //hack to fix dat.gui colors in chrome
+
+        }, this);
+        return volume;
+    },
+
+    _doVolume2d: function (info) {
+        var volume = new X.volume();
+        volume.file = _.map(this.files.models, function (file) {
+            return file.downloadUrl() + '/' + file.name();
+        });
+
+        this.renderer.onShowtime = _.bind(function () {
+            /*if (_.has(info, 'window')) {
+                volume.windowLower = info.window[0];
+                volume.windowHigh = info.window[1];
+            }*/
+
+            //volume.lowerThreshold = info.lowerThreshold || 0;
+
+            var volumeGui = this.gui.addFolder('Volume display');
+
+            volumeGui.add(volume, 'lowerThreshold', volume.min, volume.max).name('Threshold min');
+            volumeGui.add(volume, 'upperThreshold', volume.min, volume.max).name('Threshold max');
+            volumeGui.add(volume, 'windowLow', volume.min, volume.max).name('Window min').listen();
+            volumeGui.add(volume, 'windowHigh', volume.min, volume.max).name('Window max').listen();
+            volumeGui.add(volume, 'indexX', 0, volume.range[0] - 1).name('X slice').listen();
+            volumeGui.add(volume, 'indexY', 0, volume.range[1] - 1).name('Y slice').listen();
+            volumeGui.add(volume, 'indexZ', 0, volume.range[2] - 1).name('Z slice').listen();
+            /*volumeGui.add(this.renderer, 'orientation', ['Axial', 'Sagittal', 'Coronal']).name('Orientation').onChange(_.bind(function () {
+            }, this));*/
+            window.r = this.renderer;
+
+            volumeGui.open();
+
+        }, this);
+        return volume;
     },
 
     // [0, 1] -> [0, 255]
